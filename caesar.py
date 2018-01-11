2b62fc9f65994a37158690bbdb45f62d36bec678
ixtWuko <ixtwuko@outlook.com>
2018-01-11 17:32:53 +0800

    caesar's encryption function

---
+++ caesar.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# 凯撒密码
+
+def caesar_encode(string_to_encode, shift = 3):
+    result = ''
+    for char in string_to_encode:
+        if char.isalpha():
+            if char.islower():
+                temp = (ord(char) - ord('a') + shift) % 26 + ord('a')
+            else:
+                temp = (ord(char) - ord('A') + shift) % 26 + ord('A')
+            temp_char = chr(temp)
+        else:
+            temp_char = char
+        result += temp_char
+    return result
+
+def casear_decode(string_to_decode, shift = 3):
+    result = ''
+    for char in string_to_decode:
+        if char.isalpha():
+            if char.islower():
+                temp = (ord(char) - ord('a') - shift + 26) % 26 + ord('a')
+            else:
+                temp = (ord(char) - ord('A') - shift + 26) % 26 + ord('A')
+            temp_char = chr(temp)
+        else:
+            temp_char = char
+        result += temp_char
+    return result
+
+print(caesar_encode("encode this string using caesar's encryption"))
+print(casear_decode("ghfrgh wklv vwulqj xvlqj fdhvdu'v hqfubswlrq"))
